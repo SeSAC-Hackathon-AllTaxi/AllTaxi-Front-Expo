7f3007f74f82107c4f9ca9c39356529b8375e9fc
kimgoat <kimgoatlove@gmail.com>
2024-07-30 19:59:39 +0900

    feat: 현재 위치 좌표 변수 상태 관리

---
+++ src/state/locationStore.ts
@@ -0,0 +1,27 @@
+import create from "zustand";
+import * as Location from "expo-location";
+
+interface LocationState {
+  location: Location.LocationObject | null;
+  errorMsg: string | null;
+  fetchLocation: () => Promise<void>;
+}
+
+export const useLocationStore = create<LocationState>((set) => ({
+  location: null,
+  errorMsg: null,
+  fetchLocation: async () => {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        set({ errorMsg: "Permission to access location was denied" });
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      set({ location, errorMsg: null });
+    } catch (error) {
+      set({ errorMsg: "Error fetching location" });
+    }
+  },
+}));
