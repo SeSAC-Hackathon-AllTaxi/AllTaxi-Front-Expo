a4db0ae3a9264a372aec4dd5dcb18ca27665786f
kimgoat <kimgoatlove@gmail.com>
2024-07-30 20:04:40 +0900

    feat: 현재 위치 정보 초기화

--- App.tsx
+++ App.tsx
@@ -5,10 +5,14 @@ import HomeScreen from "screens/HomeScreen";
 import { ThemeProvider } from "contexts/ThemeContext";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
+import MapScreen from "screens/MapScreen";
+import DestinationScreen from "screens/DestinationScreen";
+import { useLocationStore } from "./src/state/locationStore";
 
 type RootStackParamList = {
   올택시: undefined;
   Booking: undefined;
+  목적지: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -19,6 +23,7 @@ export default function App() {
     NotoSansKR: require("./assets/fonts/NotoSansKR/NotoSansKR-Black.ttf"),
     "NotoSansKR-Bold": require("./assets/fonts/NotoSansKR/NotoSansKR-Bold.ttf"),
   });
+  const fetchLocation = useLocationStore((state) => state.fetchLocation);
 
   React.useEffect(() => {
     if (fontsLoaded) {
@@ -26,15 +31,19 @@ export default function App() {
     }
   }, [fontsLoaded]);
 
+  React.useEffect(() => {
+    fetchLocation();
+  }, []);
+
   if (!fontsLoaded) {
     return null;
   }
-
   return (
     <ThemeProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="올택시" component={HomeScreen} />
+          <Stack.Screen name="목적지" component={DestinationScreen} />
+          {/* <Stack.Screen name="올택시" component={MapScreen} /> */}
         </Stack.Navigator>
       </NavigationContainer>
     </ThemeProvider>
