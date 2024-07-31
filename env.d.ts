8ba2f94cb2f4d63b2071f10212f43f9c06af9bbf
kimgoat <kimgoatlove@gmail.com>
2024-07-31 20:33:48 +0900

    chore: setting google maps api

--- env.d.ts
+++ env.d.ts
@@ -3,4 +3,5 @@ declare module "@env" {
   export const VOICE_CHAT_URL: string;
   export const GOOGLE_SPEECH_TO_TEXT_API_KEY: string;
   export const GOOGLE_SPEECH_TO_TEXT_API_PATH: string;
+  export const GOOGLE_MAPS_iOS_API_KEY: string;
 }
