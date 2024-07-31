538eac9b1a4fe58af0708918501ca7cfc3e0a2da
kimgoat <kimgoatlove@gmail.com>
2024-07-31 15:23:08 +0900

    chore: setting api key

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,6 @@
 declare module "@env" {
   export const KAKAO_REST_API_KEY: string;
   export const VOICE_CHAT_URL: string;
+  export const GOOGLE_SPEECH_TO_TEXT_API_KEY: string;
+  export const GOOGLE_SPEECH_TO_TEXT_API_PATH: string;
 }
