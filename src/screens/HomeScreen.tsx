d8b942a40b47312d6724c7d3f489fb2deee04aed
kimgoat <kimgoatlove@gmail.com>
2024-08-01 17:48:54 +0900

    chore: 홈 화면에 마이크 버튼 적용

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,11 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { useTheme } from "contexts/ThemeContext";
 import { theme } from "constants/theme";
-import MapView from "react-native-maps";
 import MapScreen from "./MapScreen";
-import { AntDesign, FontAwesome5 } from "@expo/vector-icons";
-import { GOOGLE_API_KEY } from "@env";
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import axios from "axios";
+import { AntDesign } from "@expo/vector-icons";
+import SpeakButton from "components/SpeekButton";
 
 type RootStackParamList = {
   Home: undefined;
@@ -23,40 +20,9 @@ type Props = {
 };
 
 export default function HomeScreen({ navigation }: Props) {
-  const { theme, typography } = useTheme();
+  const { typography } = useTheme();
   const moveChatScreen = () => {
     navigation.navigate("Chat");
-  };
-  const [openSearchBar, setOpenSearchBar] = useState(false);
-  const searchInputRef = useRef<GooglePlacesAutocomplete>(null);
-
-  const handlePlaceSelect = (data: any, details: any) => {
-    // 1) kakao map api를 사용하여 목적지 상세 페이지 구현
-    // navigation.navigate("Destination", { destination: data.terms[0].value });
-
-    // 2) google map api를 사용하여 목적지 상세 페이지 구현
-    if (details) {
-      const apiRes = axios
-        .get("https://maps.googleapis.com/maps/api/place/photo", {
-          params: {
-            photoreference: details.photos[0].photo_reference,
-            maxwidth: 400,
-            key: GOOGLE_API_KEY,
-          },
-          responseType: "arraybuffer",
-        })
-        .then((res) => {
-          navigation.navigate("DestinationDetail", {
-            data: data,
-            details: details,
-            img: res.request.responseURL,
-          });
-        });
-    }
-
-    // 검색창 초기화
-    searchInputRef.current?.setAddressText("");
-    setOpenSearchBar(false);
   };
 
   return (
@@ -64,58 +30,17 @@ export default function HomeScreen({ navigation }: Props) {
       <View style={styles.map}>
         <MapScreen />
       </View>
-      {!openSearchBar && (
-        <View style={styles.info}>
-          <Text style={styles.infoText}>
-            <AntDesign name="sound" size={24} color="black" />
-            안녕하세요,
-          </Text>
-          <Text style={[typography.header]}>어디로 가고 싶으세요?</Text>
-        </View>
-      )}
-      {openSearchBar && (
-        <View style={styles.inputContainer}>
-          <GooglePlacesAutocomplete
-            ref={searchInputRef}
-            minLength={2}
-            placeholder="장소를 검색해보세요!"
-            query={{
-              key: GOOGLE_API_KEY,
-              language: "ko",
-              components: "country:kr",
-            }}
-            keyboardShouldPersistTaps={"handled"}
-            fetchDetails={true}
-            onPress={handlePlaceSelect}
-            onFail={(error) => console.log(error)}
-            onNotFound={() => console.log("no results")}
-            keepResultsAfterBlur={true}
-            enablePoweredByContainer={false}
-          />
-        </View>
-      )}
-      <View style={styles.option}>
-        <TouchableOpacity onPress={() => setOpenSearchBar(true)}>
-          <Text style={[typography.header, styles.optionText]}>
-            <AntDesign
-              name="search1"
-              size={24}
-              color={theme.colors.text.secondary}
-            />
-            검색하기
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={moveChatScreen}>
-          <Text style={[typography.header, styles.optionText]}>
-            <FontAwesome5
-              name="microphone"
-              size={24}
-              color={theme.colors.text.secondary}
-            />
-            음성으로 말하기
-          </Text>
-        </TouchableOpacity>
+
+      <View style={styles.info}>
+        <Text style={[typography.header]}>
+          <AntDesign name="sound" size={32} color="black" />
+          {"  "}
+          어디로 갈까요?
+        </Text>
       </View>
+      <TouchableOpacity onPress={moveChatScreen}>
+        <SpeakButton />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -143,37 +68,49 @@ const styles = StyleSheet.create({
     top: 0,
     left: 0,
     right: 0,
-    height: 100,
+    height: 200,
     backgroundColor: theme.colors.background,
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderBottomEndRadius: 30,
     borderBottomStartRadius: 30,
     zIndex: 1,
-    opacity: 0.9,
+    display: "flex",
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
   },
   infoText: {
     color: theme.colors.text.secondary,
     fontSize: 18,
   },
+  optionText: {
+    color: theme.colors.text.secondary,
+  },
+
   option: {
+    justifyContent: "center",
+    alignItems: "center",
     height: 130,
     position: "absolute",
-    bottom: 20,
+    bottom: 70,
     left: 20,
     right: 20,
-    backgroundColor: theme.colors.background,
     padding: 15,
     borderRadius: 10,
     elevation: 5,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
     zIndex: 1,
-    opacity: 0.9,
   },
-  optionText: {
-    color: theme.colors.text.secondary,
+  content: {
+    position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: theme.colors.background,
+    marginTop: 5,
+    fontWeight: 600,
   },
 });
