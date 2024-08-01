155cf9f50aee1436cd3b5bd2f9e0c45e5700f385
kimgoat <kimgoatlove@gmail.com>
2024-08-01 17:31:11 +0900

    chore: key setting

--- env.d.ts
+++ env.d.ts
@@ -5,4 +5,7 @@ declare module "@env" {
   export const GOOGLE_SPEECH_TO_TEXT_API_PATH: string;
   export const GOOGLE_MAPS_iOS_API_KEY: string;
   export const GOOGLE_API_KEY: string;
+  export const GOOGLE_DIRECTIONS_API_KEY: string;
+  export const NAVER_API_KEY_ID: string;
+  export const NAVER_API_KEY: string;
 }
